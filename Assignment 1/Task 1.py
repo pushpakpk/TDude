83bed747a3ea2670e2da39a81a77aea3e97fdbbd
pushpakpk <bahalepushpak@gmail.com>
2025-03-31 17:09:19 +0530

    git add README.md

---
+++ Assignment 1/Task 1.py
@@ -0,0 +1,13 @@
+num1 = input("Enter the First no: ")
+num2 = input("Enter the Second no: ")
+
+sum = float(num1)  + float(num2)
+sub = float(num1) - float(num2)
+mul = float(num1) * float(num2)
+div = float(num1) / float(num2)
+
+
+print("Addition: ",sum)
+print("Substrction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
---
+++ Assignment 1/Task2.py
@@ -0,0 +1,4 @@
+fname = input("Enter the first name: ")
+lname = input("Enter the second name: ")
+
+print(f"\nHello, {fname} {lname}! Welcome to the Python Program.")
