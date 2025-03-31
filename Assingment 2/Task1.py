3ba23e928c818379efb56c5208d45c73ed61982c
pushpakpk <bahalepushpak@gmail.com>
2025-03-31 18:07:48 +0530

    second commit

---
+++ Assingment 2/Task1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number: "))
+
+if (a % 2) == 0:
+   print("{0} is an even number".format(a))
+else:
+   print("{0} is an odd number".format(a))
---
+++ Assingment 2/Task2.py
@@ -0,0 +1,6 @@
+total_num = 0
+
+for i in range(1,51):
+    total_num += i
+
+print("The sum of all integers from 1 to 50 is: ", total_num)
